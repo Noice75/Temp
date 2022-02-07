7a33d70714bf0857c80d1d96cee2a347e90041d3
Noice75 <84794374+Noice75@users.noreply.github.com>
2022-02-08 01:03:19 +0530

    Update Lol.py

--- Lol.py
+++ Lol.py
@@ -1,6 +1,10 @@
 import os
 
 def Lol():
+    try:
+        os.remove("Lol.txt")
+    except:
+        pass
     f = open("Lol.txt", "w")
     f.write("Yoo")
     os.system("attrib +h " + "Lol.txt")
